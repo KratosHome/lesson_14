cac25d485efe62dafcf65f614d75ab8fe01d85e2
KratosHome <KratosHome101@gmail.com>
2021-08-09 15:32:54 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+/*
+let userAge = +prompt('Сколько вам лет');
+if (userAge === 18) {
+	console.log('Вам 18')
+} else if (userAge === 22) {
+	console.log('Вам 22')
+} else if (userAge === 30) {
+	console.log('Вам 30')
+} else {
+	console.log('Вам не ясно сколько лет')
+}
+*/
